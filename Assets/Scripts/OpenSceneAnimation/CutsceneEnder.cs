d5bce9327d3031a7ac4ee5a84d05827bbfaf9896
Tay Ahuva <ahuvatay@gmail.com>
2025-04-07 01:04:38 +0300

    Baked map, changed all fonts, added opening animation & new skybox

--- Assets/Scripts/OpenSceneAnimation/CutsceneEnder.cs
+++ Assets/Scripts/OpenSceneAnimation/CutsceneEnder.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using Unity.Cinemachine; // זה ה־namespace החדש ב־Cinemachine 4
 
@@ -5,12 +6,23 @@ public class CutsceneEnder : MonoBehaviour
 {
     public GameObject fakePlayer;
     public GameObject realPlayer;
+    public GameObject tom;
+    public GameObject animationCamera;
 
     public CinemachineCamera vcamIntro;
     public CinemachineCamera vcamPlayer;
 
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     public void OnCutsceneEnd()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        tom.SetActive(false);
         // מפעילים את השחקן האמיתי
         realPlayer.SetActive(true);
 
@@ -20,5 +32,6 @@ public class CutsceneEnder : MonoBehaviour
 
         // מכבים את הדמות של האנימציה
         fakePlayer.SetActive(false);
+        animationCamera.SetActive(false);
     }
 }
