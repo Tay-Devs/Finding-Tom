e7fd554ae352083a083f53a028faee29709ea138
Tay Ahuva <ahuvatay@gmail.com>
2025-04-08 15:10:26 +0300

    Dice puzzle. CM Change, Moon, Animation Image, Shadows
    Dice puzzle are now 3 dice instead of 5
    changed the CM to change differently depending on the animation or in puzzle, moon is less brighter and the lamp as well.
    added a spot light on the image in the animation start and disabled it mid animation.
    added shadows in the corridor and the dream room

--- Assets/Materials/Pause menu/PaperFoldTimeSetter.cs
+++ Assets/Materials/Pause menu/PaperFoldTimeSetter.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
 
-[ExecuteAlways]
 public class PaperFoldTimeSetter : MonoBehaviour
 {
-    [SerializeField] private Material material;
+    [SerializeField] 
+    private Material material;
+
+    [SerializeField] 
+    private bool isTimeUnscaled = true;
+
+    private float activationTime;
+
+    private void OnEnable()
+    {
+        // Store the current time when this object gets enabled
+        activationTime = isTimeUnscaled ? Time.unscaledTime : Time.time;
+    }
 
     private void Update()
     {
         if (material != null)
         {
-            material.SetFloat("_UnscaledTime", Time.unscaledTime);
+            float elapsedTime = (isTimeUnscaled ? Time.unscaledTime : Time.time) - activationTime;
+            material.SetFloat("_UnscaledTime", elapsedTime);
         }
     }
 }
